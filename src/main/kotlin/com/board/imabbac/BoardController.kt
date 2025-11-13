85d460f14c2fc54d5a64a587742492c6493ad4c9
seol-wonjun <qiops@naver.com>
2025-11-14 00:35:42 +0900

    read part

--- src/main/kotlin/com/board/imabbac/BoardController.kt
+++ src/main/kotlin/com/board/imabbac/BoardController.kt
@@ -1,6 +1,6 @@
 package com.board.imabbac
 
-import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -17,4 +17,15 @@ class BoardController (
     ) {
         boardService.create(boardDto)
     }
+
+    @GetMapping
+    fun getAll(): List<BoardDto> { //컨트롤러 딴에서도 rest나 api 딴으로 올려줘야하기 때문에 return을 정의한다
+        return boardService.getAll() //boardService를 호출하여 데이터를 가져옴
+    }
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable("id") id: Long): BoardDto { //개별값이라 List형식 안씀
+        return boardService.getById(id) //@PathVariable을 통해 id값을 받아온 뒤 boardService를 호출하여 데이터를 가져옴
+    }
+
 }
--- src/main/kotlin/com/board/imabbac/BoardService.kt
+++ src/main/kotlin/com/board/imabbac/BoardService.kt
@@ -2,6 +2,9 @@ package com.board.imabbac
 
 import org.springframework.stereotype.Service
 
+// 엔티티는 직접적으로 컨트롤러로 넘겨서 프론트와 상호작용을 하면 안됨.
+// repository에서 조회된 데이터는 엔티티로 반환이 되는데 그 엔티티를 Dto로 바꿔줘야함.
+
 @Service
 class BoardService (
     private val boardRepository: BoardRepository
@@ -15,5 +18,34 @@ class BoardService (
             name = boardDto.name
         )
         boardRepository.save(boardEntity)
+    }
+
+    //전체 조회
+    fun getAll(): List<BoardDto> {//리턴같은 경우 List형태의 BoardDto를 반환
+        val boardEntities = boardRepository.findAll() //엔티티가 리스트 형식으로 담김.
+        val boardDtoList = boardEntities.map { //map은 for문 같은 느낌. 엔티티들 다 돌려서 dto로 만듦
+            board -> BoardDto (
+                id = board.id,
+                title = board.title,
+                content = board.content,
+                name = board.name
+            )
+        }
+        return boardDtoList //컨트롤러에 넘겨줌
+    }
+
+    //개별 조회
+    fun getById(id : Long): BoardDto{ //컨트롤러가 boardDto를 받아야하므로 반환값을 여기다도 써줌
+        // 파라메터가 id로 받아오기에 findById도 id값으로 찾음
+        val boardEntity = boardRepository.findById(id).get() //boardRepository에서 BoardEntity로 데이터를 가져옴.
+        //엔티티를 Dto로 변환해줘야함.
+        //생성자를 통해서 값을 하나씩 가져옴
+        val boardDto = BoardDto (
+            id = boardEntity.id,
+            title = boardEntity.title,
+            content = boardEntity.content,
+            name = boardEntity.name
+        )
+        return boardDto
     }
 }
